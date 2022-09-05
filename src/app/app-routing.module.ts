5b1e3ab990fa51844d00b4cae4f85d2d96926fa1
CONURE-SAQEEF <muhammad.shabbir@conurets.com>
2022-09-05 11:24:01 +0500

    updated app routing module and app component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { DashboardComponent } from "./dashboard/dashboard.component";
-import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { AuthGuard } from './_helpers/auth.guard';
 
 const routes: Routes = [
     { path: '', component: DashboardComponent, canActivate: [AuthGuard] },
-    { path: 'login', component: AppComponent },
+    { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
 
-    // otherwise redirect to home
+    //otherwise redirect to home
     { path: '**', redirectTo: '' }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,28 +3,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
+import { RouterModule } from '@angular/router';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { RegisterComponent } from './register/register.component';
+import { AlertComponent } from './_components/alert.component';
 
 import { fakeBackendProvider } from './_helpers/fake_backend';
 
 import { JwtInterceptor } from './_helpers/jwt.interceptor';
 import { ErrorInterceptor } from './_helpers/error.interceptor';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DashboardComponent,
-    RegisterComponent
+    RegisterComponent,
+    LoginComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule
   ],
   providers: [
         { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
